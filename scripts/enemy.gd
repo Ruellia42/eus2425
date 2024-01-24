0c445193413e111e7223b0a79d8753faf2795b26
Nico Auerswald <auersw.nico@gmail.com>
2024-01-24 19:24:54 +0100

    add support for enemy changing direction on setup

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 
 
 @export var speed = 50
+@export var direction = 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction = 1
+
+func _ready():
+	if direction == -1:
+		scale.x *= -1
 
 func _physics_process(delta):
 	# Add the gravity.
