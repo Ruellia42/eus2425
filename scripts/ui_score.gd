c49ae4f7299d226c9305da9492b8129558c050fe
Nadine Jachmann <nadine.jachmann@uni-bayreuth.de>
2024-09-09 18:09:12 +0200

    feat: finish lesson 1 level and start level02 for lesson 3

--- scripts/ui_score.gd
+++ scripts/ui_score.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 
-@export var label : Label
+@export var label_score : Label
 @export var label_keys: Label
 var score : int = 0
 
@@ -9,6 +9,7 @@ var score : int = 0
 func _ready():
 	SignalBus.on_coin_pickup.connect(handle_coin_pickup)
 	SignalBus.on_keys_changed.connect(handle_key_change)
+	score = 0
 	update_visuals()
 	
 	
@@ -20,4 +21,4 @@ func handle_key_change(keys):
 	label_keys.text = str(keys).pad_zeros(2)
 
 func update_visuals():
-	label.text = str(score).pad_zeros(2)
+	label_score.text = str(score).pad_zeros(2)
